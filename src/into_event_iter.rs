c9bb7cd638cc72695acdb8947f6ead7102475991
Jeron Aldaron Lau <aldaronlau@gmail.com>
2024-06-22 17:00:15 -0500

    Remove `'static` requirement on `IntoEventIterator`

--- src/into_event_iter.rs
+++ src/into_event_iter.rs
@@ -1,25 +1,24 @@
 use crate::EventIterator;
 
-/// Trait for converting something into a `'static` event iterator
+/// Trait for converting something into an event iterator
 ///
-/// This is automatically implemented for all `'static` types that implement
+/// This is automatically implemented for all types that implement
 /// [`EventIterator`].
-pub trait IntoEventIterator {
+pub trait IntoEventIterator<'a> where Self: 'a {
     /// The type of the event yielded by the event iterator
     type Event<'me>
     where
-        Self: 'me;
+        Self: 'me + 'a;
     /// The type of the resulting event iterator
-    type IntoEventIter: for<'me> EventIterator<Event<'me> = Self::Event<'me>>
-        + 'static;
+    type IntoEventIter: EventIterator<Event<'a> = Self::Event<'a>>;
 
     /// Convert `self` into an event iterator.
     fn into_event_iter(self) -> Self::IntoEventIter;
 }
 
-impl<I> IntoEventIterator for I
+impl<'a, I> IntoEventIterator<'a> for I
 where
-    I: EventIterator + 'static,
+    I: EventIterator + 'a,
 {
     type Event<'me> = I::Event<'me> where Self: 'me;
     type IntoEventIter = Self;
